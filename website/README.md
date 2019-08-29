0fc50fba7642f1140e332e7a37379f03b9bf2a3c
eankeen <24364012+eankeen@users.noreply.github.com>
2019-08-29 15:02:00 -0700

    feat: initialize website getting development tools setup

---
+++ website/README.md
@@ -0,0 +1,19 @@
+# website
+
+serves as a frontend to the skin sense backend machine learning classification service.
+
+## usage
+
+be sure [yarn](https://yarnpkg.com/en) is installed
+
+### development
+
+setup development server at `localhost:8080`. it automatically reloads the server on file changes.
+
+```sh
+yarn serve
+```
+
+### production
+
+no current workflow for building frontend for production.
---
+++ website/rollup.config.js
@@ -0,0 +1,22 @@
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import postcss from 'rollup-plugin-postcss'
+
+export default {
+  input: "src/main.js",
+  output: {
+    file: "public/bundle.js",
+    format: "iife",
+    sourcemap: true
+  },
+  plugins: [
+    postcss({ extract: true }),
+    resolve(),
+    commonjs(),
+    serve({ contentBase: "", port: 8080 }),
+    livereload()
+  ]
+};
+

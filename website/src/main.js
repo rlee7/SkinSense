54867ccabcaf6b892a390eb0ee67e597c0b4c0ad
eankeen <24364012+eankeen@users.noreply.github.com>
2019-08-29 18:12:24 -0700

    polish: frontend website design

--- website/src/main.js
+++ website/src/main.js
@@ -1,5 +1,40 @@
 import "./style.css";
 
-document.querySelector("button").addEventListener("click", e => {
-  console.log("button clicked");
+const btn = document.querySelector("button");
+const inpt = document.querySelector("input");
+console.log(btn);
+
+btn.addEventListener("click", e => {
+  inpt.click();
 });
+
+btn.addEventListener("dragenter", e => {
+  e.stopPropagation();
+  e.preventDefault();
+  btn.classList.toggle("expanded");
+  e.dataTransfer.dropEffect = "copy";
+});
+
+btn.addEventListener("dragover", e => {
+  e.stopPropagation();
+  e.preventDefault();
+  let data = e.dataTransfer.getData("text/plain");
+  console.log(data);
+});
+
+btn.addEventListener("dragleave", e => {
+  btn.classList.toggle("expanded");
+});
+
+btn.addEventListener("drop", e => {
+  btn.classList.toggle("expanded");
+  e.stopPropagation();
+  e.preventDefault();
+
+  const dt = e.dataTransfer.files;
+
+  console.log(dt);
+});
+
+
+

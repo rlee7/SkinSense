2f644e7891745b6c0b5fcd95ee203a53ca1b72c8
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-09-03 23:28:10 -0700

    refactor: change and iterate ui

--- website/src/main.js
+++ website/src/main.js
@@ -1,40 +1,52 @@
 import "./style.css";
 
-const btn = document.querySelector("button");
-const inpt = document.querySelector("input");
+const btn = document.getElementById("drop");
+const tryItBtn = document.getElementById("try-it");
+const input = document.querySelector("input");
 console.log(btn);
 
 btn.addEventListener("click", e => {
-  inpt.click();
+  input.click();
 });
+input.addEventListener("change", () => {
+  const fileList = document.querySelector("input").files[0];
+  console.log(fileList);
+}, false);
 
 btn.addEventListener("dragenter", e => {
   e.stopPropagation();
   e.preventDefault();
-  btn.classList.toggle("expanded");
-  e.dataTransfer.dropEffect = "copy";
+  document.getElementById("drop").classList.toggle("expanded");
 });
 
 btn.addEventListener("dragover", e => {
   e.stopPropagation();
   e.preventDefault();
-  let data = e.dataTransfer.getData("text/plain");
-  console.log(data);
 });
 
-btn.addEventListener("dragleave", e => {
-  btn.classList.toggle("expanded");
-});
 
 btn.addEventListener("drop", e => {
   btn.classList.toggle("expanded");
   e.stopPropagation();
   e.preventDefault();
 
-  const dt = e.dataTransfer.files;
-
+  let dt = e.dataTransfer.files[0];
+  let fr = new FileReader()
+  dt = fr.readAsText(dt);
   console.log(dt);
 });
 
+btn.addEventListener("dragleave", e => {
+  btn.classList.toggle("expanded");
+});
 
 
+let tryItBtnFn = () => {
+  tryItBtn.removeEventListener("click", tryItBtnFn);
+  document.getElementById("example").classList.add("hide");
+  
+  setTimeout(() => {
+    document.getElementById("upload").classList.add("show");
+  }, 100)
+}
+tryItBtn.addEventListener("click", tryItBtnFn);

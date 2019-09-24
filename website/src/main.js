01b763f289a2201d53933a149aa121977a68b9c8
Edwin Kofler <24364012+eankeen@users.noreply.github.com>
2019-09-24 02:42:05 -0700

    revert "polish: frontend website design"
    this reverts commit 54867ccabcaf6b892a390eb0ee67e597c0b4c0ad

--- website/src/main.js
+++ website/src/main.js
@@ -1,52 +1,12 @@
 import "./style.css";
 
-const btn = document.getElementById("drop");
-const tryItBtn = document.getElementById("try-it");
+const btn = document.querySelector("button");
 const input = document.querySelector("input");
 console.log(btn);
 
-btn.addEventListener("click", e => {
-  input.click();
-});
+btn.addEventListener("click", () => input.click());
+
 input.addEventListener("change", () => {
   const fileList = document.querySelector("input").files[0];
   console.log(fileList);
 }, false);
-
-btn.addEventListener("dragenter", e => {
-  e.stopPropagation();
-  e.preventDefault();
-  document.getElementById("drop").classList.toggle("expanded");
-});
-
-btn.addEventListener("dragover", e => {
-  e.stopPropagation();
-  e.preventDefault();
-});
-
-
-btn.addEventListener("drop", e => {
-  btn.classList.toggle("expanded");
-  e.stopPropagation();
-  e.preventDefault();
-
-  let dt = e.dataTransfer.files[0];
-  let fr = new FileReader()
-  dt = fr.readAsText(dt);
-  console.log(dt);
-});
-
-btn.addEventListener("dragleave", e => {
-  btn.classList.toggle("expanded");
-});
-
-
-let tryItBtnFn = () => {
-  tryItBtn.removeEventListener("click", tryItBtnFn);
-  document.getElementById("example").classList.add("hide");
-  
-  setTimeout(() => {
-    document.getElementById("upload").classList.add("show");
-  }, 100)
-}
-tryItBtn.addEventListener("click", tryItBtnFn);
